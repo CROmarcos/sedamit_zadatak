2a771815e4846ee4bd50bb415b0b8043602842ce
Marko Pavlović <markopavlovic2010@hotmail.com>
2022-02-19 19:24:45 +0100

    Kreirano sučelje za račun

---
+++ src/Interface.tsx
@@ -0,0 +1,21 @@
+import { EnumType } from "typescript";
+
+interface IRacun {
+    racun: {
+        id: number,
+        broj_racuna: string,
+        redni_broj_racuna: number,
+        smjer: boolean | EnumType | string,
+        datum_racuna: Date,
+        rok_placanja?: Date,
+        naziv_partnera: string,
+        adresa_partnera?: string,
+        oib?: string,
+        iznos_prije_poreza: number,
+        porez?: number,
+        iznos_poreza?: number,
+        cijena_s_porezom: number
+    }
+}
+
+export default IRacun
