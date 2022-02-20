d8cf9c3e0bcb9e9d9e5d90f93bef709edb821e40
Marko Pavlović <markopavlovic2010@hotmail.com>
2022-02-20 19:32:01 +0100

    Kreirani potrebni elementi za unos

--- src/Components/DodajRacun/DodajRacun.tsx
+++ src/Components/DodajRacun/DodajRacun.tsx
@@ -1,9 +1,19 @@
 import IRacun from "../../Interface"
 
-const DodajRacun=()=>{
-    return(
-        <div>
-            
+const DodajRacun = () => {
+    return (
+        <div className="DodajRacun">
+            <div className="DodajRacun__unos">
+                <input type="text" name="broj_racuna" />
+                <input type="date" name="datum_racuna" />
+                <input type="date" name="rok_placanja" />
+                <input type="text" name="naziv_partnera" />
+                <input type="text" name="adresa_partnera" />
+                <input type="text" name="oib" />
+                <input type="text" name="iznos_prije_poreza" />
+                <input type="text" name="porez" /></div>
+            <button>Izračunaj porez</button>
+            <button>Podnesi račun</button>
         </div>
     )
 }
--- src/Pages/NoviRacun.tsx
+++ src/Pages/NoviRacun.tsx
@@ -1,7 +1,10 @@
-const NoviRacun=()=>{
-    return(
+import DodajRacun from "../Components/DodajRacun/DodajRacun"
+
+const NoviRacun = () => {
+    return (
         <div>
             <h2>Napravi novi račun:</h2>
+            <DodajRacun />
         </div>
     )
 }
